4b5da9a4350c10f255b6d0d89fffdb67d4aa1ebf
iradtaufique <iradukunda.ta@gmail.com>
2023-12-29 14:12:46 -0600

    allow user to specify ports and Host to scan

--- advancedPortScan.py
+++ advancedPortScan.py
@@ -3,6 +3,34 @@
 from socket import *
 import optparse
 from threading import *
+from  termcolor import colored
+
+def connScan(tgtHost, tgtPort):
+	try:
+		sock = socket(AF_INET, SOCK_STREAM)
+		sock.connect((tgtHost, tgtPort))
+		print(colored(f"Port {tgtPort} is Open", 'green'))
+	except:
+		print(colored(f"Port {tgtPort} is Closed", 'yellow'))
+
+	finally:
+		sock.close()
+
+def portScan(tgtHost, tgtPorts):
+	try:
+		tgtIp = gethostbyname(tgtHost)
+	except:
+		print(f"Unknown Host {tgtHost}")
+
+	try:
+		tgtName = gethostbyaddr(tgtIp)
+		print(f"Scan Results for: {tgtName[0]}")
+	except:
+		print(f"Scan Result for: {tgtIp}")
+
+	for tgtPort in tgtPorts:
+		t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+		t.start()
 
 
 def main():
@@ -15,5 +43,5 @@ def main():
 	if (tgtHost == None) | (tgtPorts[0] == None):
 		print( parser.usage)
 		exit(0)
-
+	portScan(tgtHost, tgtPorts)
 main()
