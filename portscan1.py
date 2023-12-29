f748d2006f73ef8d057c8c53ec94d6ba5ee847d9
iradtaufique <iradukunda.ta@gmail.com>
2023-12-28 23:25:05 -0600

    Allow user to specify the Host IP and Port number to scan

--- portscan1.py
+++ portscan1.py
@@ -5,8 +5,8 @@ import socket
 # socket.AF_INET is used to connect for IPV4
 # socket.SOCK_STREAM used for TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.0.159"
-port = 5000
+host = input("Enter The Host IP Address to Scan: ")
+port = int(input("Enter The Port To Scan: "))
 
 def portScanner(port):
 	if sock.connect_ex((host, port)):
