6481f243c6f8d7f2bedf2e12919c027242544ca6
iradtaufique <iradukunda.ta@gmail.com>
2023-12-29 11:00:09 -0600

    added for loop and termcolor library, for loop for scanning range of ports and termcolor for showing open and closed port in different colors

--- portscan1.py
+++ portscan1.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 
 import socket
+from termcolor import colored
 
 # socket.AF_INET is used to connect for IPV4
 # socket.SOCK_STREAM used for TCP
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("Enter The Host IP Address to Scan: ")
-port = int(input("Enter The Port To Scan: "))
+#port = int(input("Enter The Port To Scan: "))
+
+
 
 def portScanner(port):
 	if sock.connect_ex((host, port)):
-		print ('Port ' + str(port) + ' is Closed')
+		print(colored(f"Port {port} is Closed", 'yellow'))
 	else:
-		print ('Port ' + str(port) + ' is Open')
+		print (colored(f"Port {port} is Open", 'green'))
 
-portScanner(port)
+# for loop that allow to scann more than one port at time
+for port in range(5000,8000):
+	portScanner(port)
+
